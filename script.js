219638fdbc815ca55dcf50193a9e89a72516d9f7
Corey Smith <CSSmith1997@gmail.com>
2020-10-24 21:02:28 -0600

    Finished the multiple functions to append the HTML, display the temps. Changed the HTML to be better formatted for appending.

--- script.js
+++ script.js
@@ -1,30 +1,84 @@
-var searchData = "";
-function citySearch(searchData) {
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchData + "&appid=a6364729079252b0dc5b1f4530452fb4"
-$.ajax({
-    url:queryURL,
-    method: "GET",
-}).then(function(response){
-    console.log(response);
-})
-}
-$("#searchBtn").on("click", function(e){
-e.preventDefault();
-searchData = $('#searchInput').val();
-citySearch(searchData);
-console.log(searchData);
-});
+var searchHistory = JSON.parse(localStorage.getItem("cities")) || [];
 
-citySearch();
-function previousCity(){
-    $("#searchBtn").on("click", function(save){
-        save.preventDefault();
-        var previousCity = [];
-        cityLast = $('#searchInput').val().trim();
-        localStorage.setItem("#searchInput", JSON.stringify(previousCity));
-        console.log(localStorage);
-        var retrievedData = localStorage.getItem(previousCity);
-        var cityLast2 = JSON.parse(retrievedData);
-        console.log(cityLast2.length);
-    })
-}
+
+    $("#searchBtn").on('click', function(){
+        var searchData = $("#searchInput").val().trim();
+        console.log(searchData);
+        callWeatherTemps(searchData);
+    });
+
+    function callWeatherTemps(city) {
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=a6364729079252b0dc5b1f4530452fb4";
+        $.ajax({
+            url:queryURL,
+            method: "GET",
+        }).then(function(response){
+            searchHistory.unshift(response.name)
+            localStorage.setItem('cities', JSON.stringify(searchHistory))
+            displaysearchHistory(searchHistory)
+            $("#weather-append").empty();
+            var farenheit = (
+                (parseInt(response.main.temp - 273.15) *9) / 5 + 32).toFixed();
+                var humidity = response.main.humidity + "%";
+                var wind = response.main.speed;
+                var cardBody = $("<div>").addClass("card-body");
+                var cardTitle = $("<h3>").addClass("card-title").text(response.name + " " + new Date().toLocaleDateString());
+                var cardTemp = $("<p>").text("Temperature: " + farenheit);
+                var cardHumidity = $("<p>").text("Wind Speed: " + humidity);
+                var cardWind = $("<p>").text("Wind Speed: " + wind);
+                $("weather-append").append(
+                    cardBody,
+                    cardTitle,
+                    cardTemp,
+                    cardHumidity,
+                    cardWind);
+                    callUVIndex(response.coord.lat, response.coord.lon);
+                    callFiveDay(response.coord.lat, response.coord.lon);
+                });
+            }
+                function callUVIndex(lat, lon) {
+                    var queryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon-" + lon + "&appid=a6364729079252b0dc5b1f4530452fb4";
+                    $.ajax({
+                        url:queryURL,
+                        method: "GET",
+                    }).then(function(response){
+                        console.log(response);
+                        var uv = response.value;
+                        console.log(uv);
+                        var cardUV = $("<p>").text("UV Index: " + uv);
+                        $("#weather-append").append(cardUV);
+                    });
+                }
+                function callFiveDay (lat, lon) {
+                    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat-" + lat + "&lon-" + lon + "&appid=a6364729079252b0dc5b1f4530452fb4";
+                    $.ajax({
+                        url: queryURL,
+                        method: "GET",
+                    }).then(function(response){
+                        console.log(response);
+                        var dayArray =response.daily;
+                        for (var i = 0; i <5; i ++){
+                            var forecastWeather = dayArray[i+1];
+                            var date = new Date(forecastWeather.dt * 1000);
+                            console.log(date);
+                            var farenheit = ((parseInt(forecastWeather.temp.day - 273.15) * 9) / 5 + 32).toFixed();
+                            var cardBody = $("<div>").addClass("card-body");
+                            var cardTitle = $('<h3>').addClass("card-title").text(date.toLocaleDateString());
+                            var cardTemp = $('<p>').text("Temperature: " + farenheit);
+                            $("#fiveday-append").append(cardBody, cardTitle, cardTemp);
+                        }
+                    });
+                }
+                function displaysearchHistory(cities){
+                    $("#search-history").empty();
+                    for (var i = 0; i < cities.length; i++) {
+                        let city = cities[i];
+                        var li = $("<li>").addClass("list-group-item").text(city);
+                        li.on('click', function(){
+                            callWeatherTemps(city);
+                        })
+                    }$("#search-history").append(li);
+                }
+            displaysearchHistory(searchHistory);
+
+                
