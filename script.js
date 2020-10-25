aab6ef75666eaa84351dd9b9305ace25e9bea0a1
Corey Smith <CSSmith1997@gmail.com>
2020-10-24 19:02:06 -0600

    Added a function to store and recall the last city input

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var searchData = "";
-
 function citySearch(searchData) {
-    var queryURL: "https://api.openweathermap.org/data/2.5/forecast?q="+ searchData + "&appid=a6364729079252b0dc5b1f4530452fb4"
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchData + "&appid=a6364729079252b0dc5b1f4530452fb4"
 $.ajax({
     url:queryURL,
     method: "GET",
@@ -17,3 +16,15 @@ console.log(searchData);
 });
 
 citySearch();
+function previousCity(){
+    $("#searchBtn").on("click", function(save){
+        save.preventDefault();
+        var previousCity = [];
+        cityLast = $('#searchInput').val().trim();
+        localStorage.setItem("#searchInput", JSON.stringify(previousCity));
+        console.log(localStorage);
+        var retrievedData = localStorage.getItem(previousCity);
+        var cityLast2 = JSON.parse(retrievedData);
+        console.log(cityLast2.length);
+    })
+}
